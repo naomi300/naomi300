6ac5667d34a05a14749709203f8e6861cbb28bcd
naomi300 <n949124@gmail.com>
2022-06-03 10:39:04 +0300

    vehicles.py

---
+++ classes/vehicles.py
@@ -0,0 +1,26 @@
+# create a vehicle class without any variables and methods have maximum speed and miliage
+from turtle import speed
+
+
+class vehicle:
+    def __init__(self, _speed, _miliage):
+        self.speed= _speed
+        self.miliage= _miliage
+toyota= vehicle("100 km/hr","10000")
+print(toyota.speed,toyota.miliage)
+
+
+
+
+ 
+
+
+        
+        
+
+
+
+
+
+
+        
