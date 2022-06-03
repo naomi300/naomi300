f311f722d9985d4fc4522340c5be4a6f1f391acb
naomi300 <n949124@gmail.com>
2022-06-03 12:52:04 +0300

    lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,10 @@
 njeri_fav_food=["beef","choma","ugali"]
-jane_fav_food=["chapati","rice","pork"]
+jane_fav_food=["mango","rice","pork"]
 
 fav_food={
     "njeri":["beef","choma","ugali"],
 
-    "jane":["chapati","rice","pork"]
+    "jane":["mango","rice","pork"]
 }
 print(njeri_fav_food )
 print(jane_fav_food)
