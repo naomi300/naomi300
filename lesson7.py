afa1d9a0da00730bf2bf8ab18eefd72006214dfe
naomi300 <n949124@gmail.com>
2022-06-03 12:55:31 +0300

    lesson7.py

--- lesson7.py
+++ lesson7.py
@@ -1,4 +1,4 @@
-school ="joy", "hope", "mercy", "happy"
+school ="blessing", "hope", "mercy", "happy"
 pupil ="peace","patience", "ann","mary"
 #print("i am a (pupil)[0] and i school at (school[0]")
 #print("i am a (pupil)[1] and i school at (school[1]")
