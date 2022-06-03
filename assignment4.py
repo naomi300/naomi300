ae2f15e2cecc0a7f8b917cebbde6c104e39e65cb
naomi300 <n949124@gmail.com>
2022-06-03 12:46:18 +0300

    assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -2,13 +2,13 @@
 #write a program to withdraw ksh 25000 if account balance is between ksh 10000 to 200000 30%if account bal is btwn 500000 and 1million
 #above 100000 deduct 150000
 acc_bal= input("enter accont bal")
-if (int(acc_bal))>1000000 and (int(acc_bal)<200000): 
+if (int(acc_bal))>100000 and (int(acc_bal)<200000): 
   acc_bal=acc_bal-25000
   print("we have deducted ksh 25000")
-elif(int (acc_bal))>500000 and(int(acc_bal)<1000000):
+elif(int (acc_bal))>500000 and(int(acc_bal)<100000):
     acc_bal=acc_bal-(acc_bal*0.3)
     print("we have deducted 30% from your account")
-elif(int(acc_bal)>1000000):
+elif(int(acc_bal)>100000):
     acc_bal=acc_bal-15000
     print("we have deducted 15000")
 else:
