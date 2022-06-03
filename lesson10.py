781b3cd66e2bddc7807d19c778853e21fa2bb5b0
naomi300 <n949124@gmail.com>
2022-06-03 12:19:08 +0300

    lesson10.py

--- lesson10.py
+++ lesson10.py
@@ -11,11 +11,7 @@ for number in  (0,10):
     square=number**2
     square.append(square)
 #print(square)
-# cubes=[]
-for number in (0,10):
-    cube=number**3
-    cube.append(cube)
-    #print(cube)
+
 
 
 
