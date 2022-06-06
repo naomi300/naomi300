51cfc0a4d0d6af4cd134187507e271030a35eca0
naomi300 <n949124@gmail.com>
2022-06-06 13:07:23 +0300

    operation.py

---
+++ operation.py
@@ -0,0 +1,26 @@
+from unittest import result
+
+
+def add_numbers(x,y):
+    result = x + y
+    return result
+
+
+def subtract_number(x,y):
+    result = x - y
+    return result
+
+
+
+
+def divide_number(x,y):
+    result = x / y
+    return result
+
+
+def multiply_number(x,y):
+    result = x * y
+    return result
+
+
+
