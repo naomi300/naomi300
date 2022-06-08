b30e5a5c66ad7460de2566e356eadda30b2495cf
naomi300 <n949124@gmail.com>
2022-06-08 10:03:57 +0300

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+#ask user to select which input to check number or letter
+#palindrome number is a number that when reversed it remains the same
+
+
+
+msg = input("enter a number to check if it is a palindrome   ")
+if msg==msg[::-1]:
+    print("this number is a palindrome")
+
+else:
+    print("this number is not a palindrome")
+
+msg = input("enter a letter to check palindrome  ")
+if msg==msg[::-1]:
+    print("this letter is a palindrome")
+
+else:
+    print("this letter is not a palindrome")
