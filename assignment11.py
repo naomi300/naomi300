d4d8178814ab3cb60280f8c228db678746927ec8
naomi300 <n949124@gmail.com>
2022-06-14 09:32:36 +0300

    assignment11.py

--- assignment11.py
+++ assignment11.py
@@ -1,17 +1,24 @@
 #creating a digital clock
 
-
 import time
-from tkinter import *
-canvas = Tk()
-canvas.title("Digital Clock")
-canvas.geometry("350x200")
-canvas.resizable(1,1)
-label = Label(canvas, font=("New times roman", 30, 'bold'), bg="blue", fg="white", bd =30)
-label.grid(row =0, column=1)
+
+from tkinter import*
+window = Tk()
+window.title("digital clock")
+window.geometry("350x200")
+window.resizable(1,1)
+label =Label(window, font=("New times Roman", 30, "bold"), bg="blue",fg="white",bd= 30)
+label.grid(row=0,column=1)
 def digitalclock():
-   text_input = time.strftime("%H:%M:%S")
-   label.config(text=text_input)
-   label.after(200, digitalclock)
+    text_input = time.strftime("%H:%M:%S")
+    label.configure (text=text_input)
+    label.after(200, digitalclock)
 digitalclock()
-canvas.mainloop()
+window.mainloop()
+
+
+
+
+
+
+
