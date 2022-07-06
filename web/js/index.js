8b580fe6e10535e89a11a157dd5b6a14ee4287a4
naomi300 <n949124@gmail.com>
2022-07-06 10:53:07 +0300

    index.js

---
+++ web/js/index.js
@@ -0,0 +1,14 @@
+let message = ("HELLO WORLD")
+console.log (message)
+//Single line comment in js
+/*
+multi-line comment in js
+*/
+let num1 = 29
+let num2 = 34
+
+console.log(num1 + num2)
+function submitButton(){
+    alert("button has been clicked")
+}
+
